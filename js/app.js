b32dffe6c8b3b7472dbe92df25ed0ad9f028beac
paulmrest <paulmrest@gmail.com>
2020-05-16 22:58:45 -0700

    Lab14 - JS complete, no CSS

--- js/app.js
+++ js/app.js
@@ -13,12 +13,19 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // DONE: Fill in this instance method to save the contents of the cart to localStorage
-  window.localStorage.setItem('Cart Items', JSON.stringify(this));
+  window.localStorage.setItem('cart', JSON.stringify(this));
 };
 
-Cart.prototype.removeItem = function(item) {
-  // TODO: Fill in this instance method to remove one item from the cart.
+Cart.prototype.removeItem = function(itemToRemove) {
+  // DONE: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  for (var i = 0; i < this.items.length; i++)
+  {
+    if (this.items[i].product === itemToRemove.product)
+    {
+      this.items.splice(i, 1);
+    }
+  }
 };
 
 var CartItem = function(product, quantity) {
--- js/cart.js
+++ js/cart.js
@@ -7,8 +7,14 @@ table.addEventListener('click', removeItemFromCart);
 var cart;
 
 function loadCart() {
-  var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-  cart = new Cart(cartItems);
+  var rawJSONCart = JSON.parse(localStorage.getItem('cart')) || [];
+  var cartItemsArray = new Array();
+  for (var i = 0; i < rawJSONCart.items.length; i++) {
+    cartItemsArray.push(new CartItem(
+      rawJSONCart.items[i].product,
+      rawJSONCart.items[i].quantity));
+  }
+  cart = new Cart(cartItemsArray);
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
@@ -18,27 +24,80 @@ function renderCart() {
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+// DONE: Remove all of the rows (tr) in the cart table (tbody)
+function clearCart() {
+  var cartTBodyEls = document.getElementById('cart').getElementsByTagName('tbody');
+  for (var tBodyEl of cartTBodyEls) {
+    while (tBodyEl.firstChild) {
+      tBodyEl.removeChild(tBodyEl.firstChild);
+    }
+  }
+}
 
-// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+// DONE: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
-  // TODO: Find the table body
+  // DONE: Find the table body
+  var cartTBodyEls = document.getElementById('cart').getElementsByTagName('tbody');
+  /*
+  Having a tbody element without an Id seems like bad design to me, as there is
+  nothing stopping a table element from having multiple tbody elements.
+  There's not a good way of handling the possibility of multiple un-Id-ed tbody
+  elements, but it seems the best option is to just render the table in the first
+  one and ignore the rest. Thus the for loop below that only goes to cartTBodyEls[0].
+  */
+  for (var i = 0; i < 1; i++) {
+    var cartTBodyEl = cartTBodyEls[i];
+    //START-CONSOLE-TESTING
+    // var cartItemsLength = cart.items.length;
+    console.log(`cart typeof: ${typeof (cart)}`);
+    console.log(`cart.items typeof: ${typeof (cart.items)}`);
+    //END-CONSOLE-TESTING
+    // DONE: Iterate over the items in the cart
+    for (var j = 0; j < cart.items.length; j++) {
+      // DONE: Create a TR
+      var newRowEl = document.createElement('tr');
+      // DONE: Create a TD for the delete link, quantity,  and the item
+      //delete link
+      var deleteTDEl = document.createElement('td');
+      deleteTDEl.setAttribute('id', cart.items[j].product);
+      var anchorEl = document.createElement('a');
+      anchorEl.setAttribute('href', '#delete-link');
+      anchorEl.setAttribute('id', cart.items[j].product);
+      anchorEl.addEventListener('click', removeItemFromCart);
+      anchorEl.innerText = 'X';
+      deleteTDEl.appendChild(anchorEl);
+      newRowEl.appendChild(deleteTDEl);
+      //qty td
+      var qtyTDEl = document.createElement('td');
+      qtyTDEl.innerText = cart.items[j].quantity;
+      newRowEl.appendChild(qtyTDEl);
+      //item
+      var itemTDEl = document.createElement('td');
+      itemTDEl.innerText = cart.items[j].product;
+      newRowEl.appendChild(itemTDEl);
+      // DONE: Add the TR to the TBODY and each of the TD's to the TR
+      cartTBodyEl.appendChild(newRowEl);
+    }
+  }
 
-  // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
 
 function removeItemFromCart(event) {
-
-  // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
-  // TODO: Save the cart back to local storage
-  // TODO: Re-draw the cart table
-
+  // DONE: When a delete link is clicked, use cart.removeItem to remove the correct item
+  var itemToRemove = event.target.id;
+  for (var i = 0; i < cart.items.length; i++)
+  {
+    if (cart.items[i].product === itemToRemove)
+    {
+      cart.removeItem(cart.items[i]);
+    }
+  }
+  // DONE: Save the cart back to local storage
+  cart.saveToLocalStorage();
+  // DONE: Re-draw the cart table
+  renderCart();
 }
 
 // This will initialize the page and draw the cart on screen
